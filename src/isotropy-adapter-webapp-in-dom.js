c0ce0f66ac2205c8675d899bf4556e1c3ba11a0a
Jeswin Kumar <jeswinpk@agilehead.com>
2016-03-03 17:41:59 +0530

    Switch to shared flow interfaces

--- src/isotropy-adapter-webapp-in-dom.js
+++ src/isotropy-adapter-webapp-in-dom.js
@@ -1,5 +1,5 @@
 /* @flow */
-import type { IncomingMessage, ServerResponse } from "./flow/http";;
+import type { IncomingMessage, ServerResponse } from "isotropy-interfaces/node/http";
 
 export type RenderArgsType = {
   req: IncomingMessage,
